9212d97ab7245c33f458b9df9d9e0b2f8e8965a3
Ryan Furrer <ryandotfurrer@gmail.com>
2025-01-26 15:30:03 -0500

    Initial commit for v6 on dev branch

---
+++ .prettierrc.mjs
@@ -0,0 +1,18 @@
+/** @type {import('prettier').Config} */
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: { parser: 'astro' },
+    },
+  ],
+};
